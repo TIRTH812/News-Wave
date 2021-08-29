c609d3338acfb08ab8893799e3915703bf625625
TIRTH <tirth.webdev@gmail.com>
2021-08-29 16:07:13 +0530

    Fetching the news from News-API

--- src/Components/News.js
+++ src/Components/News.js
@@ -3,59 +3,38 @@
 // props : We can not change the value of props because props are read only.
 // state : state is used when the value of variable is not fixed means the value of the variable is changed at run time.
 // Note: When you use map to iterate array the you have assign an unique key to every returned element.
+// componentDidMount: This method runs after the execution of rendor method.
 
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
 
-    articles = [
-        {
-            "source": {
-                "id": "espn-cric-info",
-                "name": "ESPN Cric Info"
-            },
-            "author": "Nisarg Rakholiya",
-            "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-            "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-            "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-            "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-            "publishedAt": "2020-04-27T11:41:47Z",
-            "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
-        },
-        {
-            "source": {
-                "id": "espn-cric-info",
-                "name": "ESPN Cric Info"
-            },
-            "author": "Tirth Patel",
-            "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-            "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-            "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-            "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-            "publishedAt": "2020-03-30T15:26:05Z",
-            "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
-        }
-    ]
-
     constructor() {
         super();
         this.state = {
-            articles: this.articles,
+            articles: [],
             loading: false
         }
+    }
+
+    async componentDidMount(){
+        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=82dff8e01a954fc388f6a74ead7f25a5";
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        this.setState({articles: parsedData.articles});
     }
 
     render(){
         return (
             <div className="container my-3">
-                <h2>NewsQuicker - Top Headlines</h2>
+                <h1>NewsQuicker - Top Headlines</h1>
                 <div className="row">
                      
                     {this.state.articles.map((element)=>{
                         return (
                             <div className="col-md-4" key={element.url}>
-                                <NewsItem title={element.title} description={element.description} imageUrl={element.urlToImage} newsUrl={element.url}/>
+                                <NewsItem title={element.title?element.title:""} description={element.description?element.description:""} imageUrl={element.urlToImage?element.urlToImage:"https://images.moneycontrol.com/static-mcnews/2021/08/Windows-11-770x433.jpg"} newsUrl={element.url}/>
                             </div>
                         )
                     })}
