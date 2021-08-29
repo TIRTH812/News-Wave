21f2c7a5f68a0d0ede0660b85609efd3ab66e5dd
TIRTH <tirth.webdev@gmail.com>
2021-08-29 13:40:10 +0530

    Various Components are created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 
 import React, { Component } from 'react'
+import NevBar from './Components/NevBar';
+import News from './Components/News';
 
 export default class App extends Component {
+
   render() {
     return (
       <div>
-        Hello My first Class Based Component
+        <NevBar/>
+        <News/>
       </div>
     )
   }
